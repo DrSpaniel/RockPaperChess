9fba9c9b458f3cf79d2df1b4c19a01c401d1d1f4
Daniel G. <80426387+DrSpaniel@users.noreply.github.com>
2025-03-16 22:57:35 -0400

    more shit while soaring in the sky
    i think i get gdscript now. at least the functions and shit. my back hurts
    
    anyway its almost done!!!

--- RPCgodot2/scenes/control.gd
+++ RPCgodot2/scenes/control.gd
@@ -3,5 +3,26 @@ extends Control
 
 
 func _on_rock_button_down() -> void:
+	var rand = (randi() % 3)
 	print("hello")
+	
 	$result.texture = load("res://images/wrong.png")
+	
+	if (rand == 0):
+		print("rock!")
+		$opponent.texture = load("res://images/rock.png")
+	elif (rand == 1):
+		print("paper!")
+		$opponent.texture = load("res://images/paper.png")
+	elif (rand == 2):
+		print("scissors!")
+		$opponent.texture = load("res://images/scissors.png")
+		
+		
+		
+		
+func rules(uInput, AiInput):	#this is the thing that decides who wins. takes opponent and user input, returns win or lose bool
+	{}
+		
+func opponentGenerate(): #when any 3 user buttons are pressed, call this and itll randomize the action and then change the image. returns AiInput
+	{}
