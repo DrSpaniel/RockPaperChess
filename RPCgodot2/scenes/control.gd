a1e9bb6dd3b584c59f51c55185e2c56f61e40b43
Daniel G. <nuclearrocketcar@gmail.com>
2025-03-25 18:22:53 -0400

    RPS section DONE!
    modulo is awesome. now to check out the chess part.

--- RPCgodot2/scenes/control.gd
+++ RPCgodot2/scenes/control.gd
@@ -3,26 +3,28 @@ extends Control
 
 
 func _on_rock_button_down() -> void:
-	var rand = (randi() % 3)
-	print("hello")
+	gameDecision(0) #0 for rock, 1 for paper, 2 for scissors
 	
-	$result.texture = load("res://images/wrong.png")
-	
-	if (rand == 0):
-		print("rock!")
-		$opponent.texture = load("res://images/rock.png")
-	elif (rand == 1):
-		print("paper!")
-		$opponent.texture = load("res://images/paper.png")
-	elif (rand == 2):
-		print("scissors!")
-		$opponent.texture = load("res://images/scissors.png")
-		
-		
-		
+func _on_scissors_button_down() -> void:
+	gameDecision(1)
+
+func _on_paper_button_down() -> void:
+	gameDecision(2)
+
+func gameDecision(uChoice): #called on any 3 button presses, recieves user input, returns opponent's image, and result
+	return rules(uChoice, opponentGenerate())
 		
 func rules(uInput, AiInput):	#this is the thing that decides who wins. takes opponent and user input, returns win or lose bool
-	{}
-		
+	if (uInput == AiInput): #tie
+		$result.texture = load("res://images/question.png") #tie
+	else:
+		if((uInput+1)%3 == AiInput):
+			$result.texture = load("res://images/right.png")
+		else:
+			$result.texture = load("res://images/wrong.png")
+	
 func opponentGenerate(): #when any 3 user buttons are pressed, call this and itll randomize the action and then change the image. returns AiInput
-	{}
+	var rand = (randi() % 3)
+	var imgDictionary = {0: "res://images/rock.png", 1: "res://images/scissors.png", 2: "res://images/paper.png"}
+	$opponent.texture = load(imgDictionary[rand])
+	return rand
